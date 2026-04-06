33d5bfd35e48f3142e74b1e5701c8ed47d18a67b
ad <wxing374@gmail.com>
2026-04-06 16:58:24 +0800

    add compact

--- lodash/admuskgit-lodash.js
+++ lodash/admuskgit-lodash.js
@@ -1,13 +1,19 @@
-var admuskgit = function() {
-  function chunk(array, size) {
+var admuskgit = {
+  chunk(array, size) {
     var result = []
     for (var i = 0; i < array.length; i += size) {
       result.push(array.slice(i, i + size))
     }
     return result
-  }
-  
-  return {
-    chunk: chunk
-  }
-}()
+  },
+  compact(array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+
+}
