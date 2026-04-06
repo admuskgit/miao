680880d4c62efad2716b97a3a5a060adc8dd1ff2
ad <wxing374@gmail.com>
2026-04-06 09:38:14 +0800

    add chunk

--- lodash/admuskgit-lodash.js
+++ lodash/admuskgit-lodash.js
@@ -1,6 +1,10 @@
 var admuskgit = function() {
-  function chunk() {
-
+  function chunk(array, size) {
+    var result = []
+    for (var i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
   }
   return {
     chunk: chunk
